5e912ee17629266fd7c1fa5d0e102de3bf7d693b
Clouddrout <91132683+ShadowPlazs@users.noreply.github.com>
2023-05-30 22:58:09 +0800

    Update definitions.py

--- synucated/definitions.py
+++ synucated/definitions.py
@@ -1,1 +1,12 @@
-
+def clear():
+  print("\033c", end='')
+  
+def fetch(a):
+  if a == "ip":
+    import socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+   
+  else:
+    raise Exception("unidentified input valuable 'a'")

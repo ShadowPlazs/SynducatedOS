aa302feb07202524dde431869800a219bb8b0b42
Clouddrout <91132683+ShadowPlazs@users.noreply.github.com>
2023-05-31 11:36:40 +0800

    Create setup.py

---
+++ synucated/setup.py
@@ -0,0 +1,17 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+    name="SynducatedOS",
+    version="0.0.10",
+    author="Lucas Chin Wu (clouddrout)",
+    author_email="",
+    description="The official library for SynducatedOS.",
+    packages=["example_library"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
